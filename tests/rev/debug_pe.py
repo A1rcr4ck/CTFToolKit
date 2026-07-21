17c7ac6c30e7debfee738a8cf46338cb313aa41e
A1rcr4ck <sarangnair35@gmail.com>
2026-07-21 20:25:28 +0530

    chore: remove generated files and update gitignore

--- tests/rev/debug_pe.py
+++
@@ -1,8 +0,0 @@
-import pefile
-
-pe = pefile.PE(r"C:\Users\saran\Downloads\ChromeSetup.exe")
-
-print(hex(pe.FILE_HEADER.Machine))
-print(pe.FILE_HEADER.Machine)
-print(hex(pe.OPTIONAL_HEADER.Magic))
-print(pe.PE_TYPE)
